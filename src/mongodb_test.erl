819519949d6a945a956a2bcdfaab30b9364f8870
Gregory Haskins <ghaskins@novell.com>
2011-06-30 17:07:34 -0400

    Add timing to the test
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- src/mongodb_test.erl
+++ src/mongodb_test.erl
@@ -10,30 +10,34 @@ init() ->
     mongo:connect (Host).
 
 run() ->
+    run(10000).
+
+run(Reps) ->
     {ok, Conn} = init(),
     Db = testdb,
     try
-	Args = [Conn, Db],
+	Args = [Conn, Db, Reps],
+	do_test(do_noop_inserts, Args),
 	do_test(do_single_inserts, Args),
 	do_test(do_compound_inserts, Args)
     after
 	cleanup(Conn)
     end.
 
--define(REPS, 10000).
-
-do_test(F, A) ->
+do_test(F, [_, _, Reps]=A) ->
     io:format("Launching ~p...", [F]),
     {Time, ok} = timer:tc(?MODULE, F, A),
-    io:format("done (~pus)~n", [Time]).
+    io:format("done (~pus, ~pus/op)~n", [Time, Time/Reps]).
 
-do_single_inserts(Conn, Db) ->
-    do_single_inserts(Conn, Db, ?REPS).
+do_noop_inserts(_Conn, _Db, Val) when Val =:= 0 ->
+    ok;
+do_noop_inserts(Conn, Db, Val) ->
+    do_noop_inserts(Conn, Db, Val-1).
 
-do_compound_inserts(Conn, Db) ->
+do_compound_inserts(Conn, Db, Reps) ->
     {ok, ok} = mongo:do(safe, master, Conn, Db,
 			fun() ->
-				do_compound_inserts(?REPS)
+				do_compound_inserts(Reps)
 			end
 		       ),
     ok.
