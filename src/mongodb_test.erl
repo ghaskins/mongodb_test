2795bbb0d5aad6f5c907e7faa36cb484d082dd35
Gregory Haskins <ghaskins@novell.com>
2011-07-06 07:45:37 -0400

    Add riak support
    Signed-off-by: Gregory Haskins <ghaskins@novell.com>

--- src/mongodb_test.erl
+++ src/mongodb_test.erl
@@ -1,13 +1,21 @@
 -module(mongodb_test).
 -compile(export_all).
 
+-record(riak, {pid}).
+-record(mongo, {conn}).
+-record(conn, {mongo, riak}).
+
 cleanup(Conn) ->
-    mongo:disconnect(Conn).
+    mongo:disconnect(Conn#conn.mongo#mongo.conn).
 
 init() ->
     application:start(mongodb),
     Host = {localhost, 27017},
-    mongo:connect (Host).
+    {ok, MongoConn} = mongo:connect (Host),
+    
+    {ok, RiakPid} = riakc_pb_socket:start_link("127.0.0.1", 8087),
+
+    {ok, #conn{mongo=#mongo{conn=MongoConn}, riak=#riak{pid=RiakPid}}}.
 
 run() ->
     run(10000).
@@ -18,8 +26,9 @@ run(Reps) ->
 	     do_noop,
 	     do_async_spawn,
 	     do_sync_spawn,
-	     do_single_inserts,
-	     do_compound_inserts
+	     do_single_mongo_inserts,
+	     do_compound_mongo_inserts,
+	     do_riak_inserts
 	    ],
     try
 	[do_test(Test, [Conn, testdb, Reps]) || Test <- Tests]
@@ -30,7 +39,8 @@ run(Reps) ->
 do_test(F, [_, _, Reps]=A) ->
     io:format("Launching ~p...", [F]),
     {Time, ok} = timer:tc(?MODULE, F, A),
-    io:format("done (~p us, ~p us/op, ~p ops/sec)~n", [Time, Time/Reps, Reps/Time*1000000]).
+    io:format("done (~p us, ~p us/op, ~p ops/sec)~n",
+	      [Time, Time/Reps, Reps/Time*1000000]).
 
 do_noop(_Conn, _Db, Val) when Val =:= 0 ->
     ok;
@@ -56,35 +66,44 @@ do_sync_spawn(Conn, Db, Val) ->
     end,
     do_sync_spawn(Conn, Db, Val-1).
 
-do_compound_inserts(Conn, Db, Reps) ->
-    {ok, ok} = mongo:do(safe, master, Conn, Db,
+do_compound_mongo_inserts(Conn, Db, Reps) ->
+    MConn = Conn#conn.mongo#mongo.conn,
+    {ok, ok} = mongo:do(safe, master, MConn, Db,
 			fun() ->
-				do_compound_inserts(Reps)
+				do_compound_mongo_inserts(Reps)
 			end
 		       ),
     ok.
 
-do_compound_inserts(Val) when Val =:= 0 ->
+do_compound_mongo_inserts(Val) when Val =:= 0 ->
     ok;
-do_compound_inserts(Val) ->
-    ll_insert(Val),
-    do_compound_inserts(Val-1).
+do_compound_mongo_inserts(Val) ->
+    ll_mongo_insert(Val),
+    do_compound_mongo_inserts(Val-1).
 
-do_single_inserts(_Conn, _Db, Val) when Val =:= 0 ->
+do_single_mongo_inserts(_Conn, _Db, Val) when Val =:= 0 ->
     ok;
-do_single_inserts(Conn, Db, Val) ->
+do_single_mongo_inserts(Conn, Db, Val) ->
     do_single_insert(Conn, Db, Val),
-    do_single_inserts(Conn, Db, Val-1).
+    do_single_mongo_inserts(Conn, Db, Val-1).
 
 do_single_insert(Conn, Db, Val) ->
-    mongo:do(safe, master, Conn, Db,
+    MConn = Conn#conn.mongo#mongo.conn,
+    mongo:do(safe, master, MConn, Db,
 	     fun() ->
-		     ll_insert(Val)
+		     ll_mongo_insert(Val)
 	     end
 	    ).
 
-ll_insert(Val) ->
+ll_mongo_insert(Val) ->
     mongo:insert(testcollection, ll_generate(Val)).
 				  
 ll_generate(Val) ->
     {x,Val, y,<<"foo">>, z,"bar"}.
+
+do_riak_inserts(_Conn, _Db, Val) when Val =:= 0 ->
+    ok;
+do_riak_inserts(Conn, Db, Val) ->
+    O = riakc_obj:new(<<"testdb">>, <<Val>>, ll_generate(Val)),
+    ok = riakc_pb_socket:put(Conn#conn.riak#riak.pid, O),
+    do_riak_inserts(Conn, Db, Val-1).
